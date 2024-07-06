67ee423d432939064124b7e0be73995c0b4e8c93
Anuj2117 <anujkushwaha221@gmail.com>
2024-07-06 14:03:43 +0530

    create new repo for assignment

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Kalkiposter from "./assets/Kalki.jpg";
+import Prabhas from "./assets/KalkiSide.jpg";
+import MirzapurPoster from "./assets/Mirzapur.jpg";
+import Kalinbhaiya from "./assets/Mirzapurside.jpg";
+import MoviePoster from './Movie-Poster';
+import './App.css'
+
+function App() {
+   const firstCard={
+    images:{
+      img1:Kalkiposter,
+      img2:Prabhas
+    },
+    yearandhero:"2024 , Amithab Bacchan ",
+    duration:"200 min",
+    type:"Spritual , Action ",
+    summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quaerat optio est distinctio natus explicabo nostrum facere, hic voluptates quos",
+
+   }
+   const SceondCard={
+    images:{
+      img1:MirzapurPoster,
+      img2:Kalinbhaiya
+    },
+    yearandhero:"2024 , Tripathi ji ",
+    duration:"200 min",
+    type:"Action , Drama ",
+    summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quaerat optio est distinctio natus explicabo nostrum facere, hic voluptates quos",
+
+   }
+  return (
+    <>
+      <MoviePoster information={firstCard}/>
+      <MoviePoster information={SceondCard}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Movie-Poster.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function MoviePoster(props) {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="left">
+          <div className="leftimagesection">
+            <img src={props.information.images.img1} alt="kalkiposter" id="kaliPoster" />
+            <div className="infosection">
+              <h2>Kalki</h2>
+              <p className="releaseYear">2017 , Anuj </p>
+              <p>
+                <span> {props.information.duration} </span>{props.information.type}
+              </p>
+            </div>
+          </div>
+          <div className="summary">
+            <p>
+              {props.information.summary}
+            </p>
+          </div>
+          <div className="shareIcon"></div>
+        </div>
+
+        <div className="right">
+          <img src={props.information.images.img2} alt="Prabhas" id="Prabhas" />
+        </div>
+      </div>
+    </>
+  );
+}
+export default MoviePoster;
